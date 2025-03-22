093bc3b52f0744be181042422358fcb30b4b09d2
Igor Bari <mucsi96@gmail.com>
2025-03-22 08:33:20 +0100

    deploy 5 replicas

--- hello_server/deploy.sh
+++ hello_server/deploy.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+set -e  # Exit immediately if a command exits with a non-zero status
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 docker build -t hello_server $SCRIPT_DIR
+
+# Get the image hash
+IMAGE_HASH=$(docker images --no-trunc --quiet hello_server:latest)
+
+echo "Deploying hello_server with image hash $IMAGE_HASH"
 
 helm upgrade \
   --install \
@@ -13,6 +20,8 @@ helm upgrade \
   --set servicePort=3080 \
   --set imagePullPolicy=Never `# needed only for local development` \
   --set serviceType=LoadBalancer `# needed only for local development` \
+  --set replicaCount=5 \
+  --set env.IMAGE_HASH=$IMAGE_HASH `# forces new deployment in case image hash changed` \
   --wait \
   --timeout 40s \
   hello-server \
--- hello_server/index.js
+++ hello_server/index.js
@@ -1,19 +1,36 @@
-const http = require('http');
+const http = require("http");
+const os = require("os");
 
 const port = 8080;
+let requestCount = 0;
 
 const server = http.createServer((req, res) => {
-  console.log(`Request received for ${req.url}`);
+  requestCount++;
 
-  if (req.url === '/health' && req.method === 'GET') {
+  if (req.url === "/health" && req.method === "GET") {
     res.writeHead(200);
-    res.end('Health check passed!');
+    res.end("Health check passed!");
     return;
   }
-  
+
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+  res.setHeader("Content-Type", "text/html");
+
+  const osDetails = `
+    <html>
+      <head>
+        <meta http-equiv="refresh" content="1">
+      </head>
+      <body>
+        <p>Image hash: ${process.env.IMAGE_HASH}</p>
+        <p>Hostname: ${os.hostname()}</p>
+        <p>IP Address: ${os.networkInterfaces().eth0[0].address}</p>
+        <p>Requests served: ${requestCount}</p>
+      </body>
+    </html>
+  `;
+
+  res.end(osDetails);
 });
 
 server.listen(port, () => {
@@ -21,10 +38,10 @@ server.listen(port, () => {
 });
 
 // Graceful shutdown on SIGTERM signal
-process.on('SIGTERM', () => {
-  console.info('Received SIGTERM signal. Closing server gracefully.');
+process.on("SIGTERM", () => {
+  console.info("Received SIGTERM signal. Closing server gracefully.");
   server.close(() => {
-    console.log('Server closed. Exiting process.');
+    console.log("Server closed. Exiting process.");
     process.exit(0);
   });
 });
